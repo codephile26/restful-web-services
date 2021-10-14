d07229f560d56dfb88e6e038a4bfee7cbbf45da2
shahd <dimple.shah@tomtom.com>
2021-10-14 12:16:45 +0530

    Different types of API versioning

--- src/main/java/com/driver/restfulwebservices/versioning/VersioningController.java
+++ src/main/java/com/driver/restfulwebservices/versioning/VersioningController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class VersioningController {
 
     //option 1 of implementing versioning - using different uri
+    //uri versioning
     @GetMapping("v1/person")
     public PersonV1 personV1() {
         return new PersonV1("Bob Charlie");
@@ -18,6 +19,7 @@ public class VersioningController {
     }
 
     //option 2 of implementing versioning - using request params
+    //Request Param versioning
     @GetMapping(value = "person/param",params="version=1")
     public PersonV1 personV1Param(){
         return new PersonV1("Charlie Chaplin");
@@ -28,7 +30,8 @@ public class VersioningController {
         return new PersonV2(new Name("Charlie","Chaplin"));
     }
 
-    //option 2 of implementing versioning - using request params
+    //option 3 of implementing versioning - using headers
+    //Header type version
     @GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
     public PersonV1 headerV1() {
         return new PersonV1("Bob Charlie");
@@ -36,6 +39,18 @@ public class VersioningController {
 
     @GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
     public PersonV2 headerV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    //option 4 of implementing versioning - using produces - Content Negotiation Approach
+    //AKA - Mime type versioning
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 producesV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 producesV2() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 }
